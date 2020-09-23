e1aee6b79875ddd1a37889aeb87bb88d0e5304ee
duc <hohuynhduca7bh123@gmail.com>
2020-09-23 23:00:40 +0700

    done 30%

--- inspirationApp/lib/main.dart
+++ inspirationApp/lib/main.dart
@@ -15,6 +15,68 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black87,
+            ),
+            onPressed: null),
+      ),
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Find Your',
+                  style: TextStyle(color: Colors.black87, fontSize: 25),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'Inspiration',
+                  style: TextStyle(color: Colors.black87, fontSize: 40),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black87,
+                      ),
+                      hintText: "Search you're looking for",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
