9b02158a09c4976e81a38092f4d5bd04bdcf165c
duc <hohuynhduca7bh123@gmail.com>
2020-09-25 15:47:19 +0700

    page view

--- TripApp/lib/main.dart
+++ TripApp/lib/main.dart
@@ -14,10 +14,37 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  PageController _pageController;
+
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+        body: PageView(
+      children: <Widget>[
+        createPage(),
+        createPage(),
+        createPage(),
+      ],
+    ));
+  }
+
+  Widget createPage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/one.jpg'),
+        ),
+      ),
     );
   }
 }
