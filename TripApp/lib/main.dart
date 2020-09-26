8384ae1b698bfb54fe197556dde7ffdbd19b0fb4
duc <hohuynhduca7bh123@gmail.com>
2020-09-26 15:17:12 +0700

    add star

--- TripApp/lib/main.dart
+++ TripApp/lib/main.dart
@@ -32,19 +32,19 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
         body: PageView(
       children: <Widget>[
-        createPage(),
-        createPage(),
-        createPage(),
-        createPage(),
+        createPage('assets/images/one.jpg'),
+        createPage('assets/images/two.jpg'),
+        createPage('assets/images/three.jpg'),
+        createPage('assets/images/four.jpg'),
       ],
     ));
   }
 
-  Widget createPage() {
+  Widget createPage(image) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/one.jpg'),
+          image: AssetImage(image),
           fit: BoxFit.cover,
         ),
       ),
@@ -52,9 +52,128 @@ class _HomepageState extends State<Homepage> {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.bottomRight,
+            stops: [0.3, 0.9],
             colors: [
-              Colors.black.withOpacity(.9),
+              Colors.black.withOpacity(.6),
               Colors.black.withOpacity(.1),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    '1',
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '/4',
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'USA',
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(
+                            right: 5,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            right: 5,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            right: 5,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            right: 5,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            right: 5,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                        Text(
+                          '4.0',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white70,
+                          ),
+                        ),
+                        Text(
+                          '(5600)',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
